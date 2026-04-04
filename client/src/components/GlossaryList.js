91dc616d07e91dcfce0047064fc68ad057136e8b
pasewalck <pasewalck@posteo.net>
2026-04-04 15:16:17 +0200

    fix: use html in frontend

--- client/src/components/GlossaryList.js
+++ client/src/components/GlossaryList.js
@@ -2,53 +2,46 @@ import React from 'react';
 import { List, ListItem, ListItemText, Typography } from '@mui/material';
 
 const GlossaryList = React.memo(({ terms, searchQuery, selectedTerm }) => {
-  const highlightText = (text, highlight) => {
+  const highlightHTML = (html, highlight) => {
     if (typeof highlight === 'string' && !highlight.trim()) {
-      return <span>{text}</span>;
+      return html;
     }
 
     const regex = new RegExp(`(${highlight})`, 'gi');
-    const parts = text.split(regex);
-    return (
-      <span>
-        {parts.filter(String).map((part, i) =>
-          regex.test(part) ? (
-            <mark key={i} style={{ backgroundColor: '#F7B377', padding: 0 }}>
-              {part}
-            </mark>
-          ) : (
-            <span key={i}>{part}</span>
-          )
-        )}
-      </span>
-    );
+    return html.replace(regex, '<mark style="background-color: #F7B377; padding: 0;">$1</mark>');
   };
 
   return (
     <List> {
       selectedTerm !== null ?
-      <ListItem key={0} sx={{ pb: 2 }}>
+        <ListItem key={0} sx={{ pb: 2 }}>
           <ListItemText primary={
-            <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 0.5 }}>
-              {highlightText(selectedTerm.header, searchQuery)}
-            </Typography>}
+            <Typography
+              variant="h6"
+              sx={{ fontWeight: 'bold', mb: 0.5 }}
+              dangerouslySetInnerHTML={{ __html: highlightHTML(selectedTerm.header, searchQuery) }}
+            />}
             secondary={
-              <Typography variant="body1">
-                {highlightText(selectedTerm.definition, searchQuery)}
-              </Typography>} />
+              <Typography
+                variant="body1"
+                dangerouslySetInnerHTML={{ __html: highlightHTML(selectedTerm.definition, searchQuery) }}
+              />} />
         </ListItem>
-      
-      : terms.map((term, index) => (
-        <ListItem key={index} sx={{ pb: 2 }}>
-          <ListItemText primary={
-            <Typography variant="h6" sx={{ fontWeight: 'bold', mb: 0.5 }}>
-              {highlightText(term.header, searchQuery)}
-            </Typography>}
-            secondary={
-              <Typography variant="body1">
-                {highlightText(term.definition, searchQuery)}
-              </Typography>} />
-        </ListItem>))
+
+        : terms.map((term, index) => (
+          <ListItem key={index} sx={{ pb: 2 }}>
+            <ListItemText primary={
+              <Typography
+                variant="h6"
+                sx={{ fontWeight: 'bold', mb: 0.5 }}
+                dangerouslySetInnerHTML={{ __html: highlightHTML(term.header, searchQuery) }}
+              />}
+              secondary={
+                <Typography
+                  variant="body1"
+                  dangerouslySetInnerHTML={{ __html: highlightHTML(term.definition, searchQuery) }}
+                />} />
+          </ListItem>))
     }
     </List>
   );
