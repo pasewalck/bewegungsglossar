28a2ad6f1562290acdd644155dbd0d4a74cb3aea
pasewalck <pasewalck@posteo.net>
2026-04-04 13:19:37 +0200

    fix: fix path resolution using fileURLToPath

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,13 @@ import terms from './services/terms.js';
 import env from './utils/env.js';
 import path from 'path';
 
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const fastify = Fastify()
 
-const clientBuildPath = path.join(import.meta.url, '../', '../', 'client', 'build')
+const clientBuildPath = path.join(__dirname, '../', 'client', 'build')
 if (fs.existsSync(path.join(clientBuildPath, "index.html"))) {
     console.log("Serving client build ...")
     fastify.register(fastifyStatic, {
@@ -30,4 +34,8 @@ fastify.get('/api/terms/:term', async (request, reply) => {
     reply.send(await terms.getTerm(request.params.term))
 });
 
-fastify.listen({ port: 3002 });
+fastify.setNotFoundHandler((request, reply) => {
+    reply.sendFile('index.html');
+});
+
+fastify.listen({ host: '0.0.0.0', port: 3002 });
